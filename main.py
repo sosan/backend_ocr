1ac7e163daa6d5be2e30d5be93c6095d7955d9d2
jose <josloe@gmail.com>
2020-08-24 19:33:42 +0200

    remote debug heriku

--- main.py
+++ main.py
@@ -54,29 +54,33 @@ def test():
 
 @app.route("/api/", methods=["POST"])
 def home_post():
+    print("1")
     if "user_image" in request.files:
         try:
+            print("2")
             # Converts an uploaded image in common formats such as JPEG, PNG into text via Optical Character Recognition.
             # datafile_b64 = request.form["image"]
             tamanoarchivo_bytes = sys.getsizeof(request.files["user_image"])
+            print("3")
             if tamanoarchivo_bytes > app.config["MAX_CONTENT_LENGTH"] or tamanoarchivo_bytes <= 0:
                 return redirect("home")
-
+            print("4")
             f = request.files["user_image"]
-            
+            print("5")
             nombrefile = get_random_string(12) + "-" + datetime.utcnow().strftime("%d-%b-%Y-%H.%M.%S.%f_") + "." + f.content_type.split("/")[1]
-
+            print("6")
             print(os.path.join(app.config["CARPETA_SUBIDAS"], nombrefile))
             with open(os.path.join(app.config["CARPETA_SUBIDAS"], nombrefile), "wb") as arch:
                 # cad_cero = datafile_b64.find(',')
                 # imagen_data64 = datafile_b64[cad_cero + 1:]
                 # arch.write(base64.decodebytes(imagen_data64.encode()))
+                print("7")
                 arch.write(f.read())
                 arch.close()
 
             # imagen_archivo = f.read().decode('UTF-8')
             # imagen_archivo = base64.b64encode(imagen_archivo)
-            
+            print("8")
             file_absolute_path = os.path.join(app.config['CARPETA_SUBIDAS'], nombrefile)
             # uploaded_file = open(file_absolute_path, 'rb',encoding=str)
             api_response = api_instance.image_ocr_post(file_absolute_path)
