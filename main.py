925b53fdfdd3a5b865801581c68584ab9c1badec
jose <josloe@gmail.com>
2020-08-28 18:43:00 +0200

    added text base64

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# PROGRAMA DONDE HACEMOS OCR DE UNA IMAGEN
+# DEVOLVEMOS EL TEXTO EN JSON
+
 import os
 import sys
 import base64
@@ -24,78 +27,78 @@ import settings
 from werkzeug.utils import secure_filename
 from flask.json import jsonify
 
+# ocr con cloudmersive
 api_instance = cloudmersive_ocr_api_client.ImageOcrApi()
-image_file = "test.png"
 
 api_instance.api_client.configuration.api_key = {}
 api_instance.api_client.configuration.api_key["Apikey"] = os.environ.get("API_KEY_OCR")
-print("{}" .format(api_instance.api_client.configuration.api_key["Apikey"]))
 
 
-
+# configuracion de la app
 app = Flask(__name__)
 app.secret_key = os.urandom(42)
 CORS(app)
-# limite 16 megas
+
+# limite 16 megas del fichero
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG"]
 
 CARPETA_SUBIDAS = os.path.abspath("static/images/archivos_subidos")
-print("capr:" + CARPETA_SUBIDAS)
+# print("capr:" + CARPETA_SUBIDAS)
 app.config["CARPETA_SUBIDAS"] = CARPETA_SUBIDAS
 
 @app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
-@app.route("/t", methods=["post"])
-def test():
-    return jsonify({"resultado": "True"})
-
-@app.route("/api/", methods=["POST"])
+@app.route("/api", methods=["POST"])
 def home_post():
-    print("1")
+    print("fff")
+    # nos enviar una imagen sin formato base64
     if "user_image" in request.files:
         try:
-            print("2")
-            # Converts an uploaded image in common formats such as JPEG, PNG into text via Optical Character Recognition.
-            # datafile_b64 = request.form["image"]
             tamanoarchivo_bytes = sys.getsizeof(request.files["user_image"])
-            print("3")
             if tamanoarchivo_bytes > app.config["MAX_CONTENT_LENGTH"] or tamanoarchivo_bytes <= 0:
                 return redirect("home")
-            print("4")
             f = request.files["user_image"]
-            print("5")
+
             nombrefile = get_random_string(12) + "-" + datetime.utcnow().strftime("%d-%b-%Y-%H.%M.%S.%f_") + "." + f.content_type.split("/")[1]
-            print("6")
-            print(os.path.join(app.config["CARPETA_SUBIDAS"], nombrefile))
+
             with open(os.path.join(app.config["CARPETA_SUBIDAS"], nombrefile), "wb") as arch:
-                # cad_cero = datafile_b64.find(',')
-                # imagen_data64 = datafile_b64[cad_cero + 1:]
-                # arch.write(base64.decodebytes(imagen_data64.encode()))
-                print("7")
                 arch.write(f.read())
                 arch.close()
 
-            # imagen_archivo = f.read().decode('UTF-8')
-            # imagen_archivo = base64.b64encode(imagen_archivo)
-            print("8")
             file_absolute_path = os.path.join(app.config['CARPETA_SUBIDAS'], nombrefile)
-            # uploaded_file = open(file_absolute_path, 'rb',encoding=str)
             api_response = api_instance.image_ocr_post(file_absolute_path)
-            print(api_response.text_result)
-            print("9")
+            # print(api_response.text_result)
             if (api_response.text_result != ""):
-                print("10.A")
                 return {"resultado": api_response.text_result}
             else:
-                print("10.B")
                 return {"resultado": "Sin Texto"}
 
         except ApiException as e:
+            # TODO: colocar el error en la db
             print("Exception when calling ImageOcrApi->image_ocr_post: %s\n" % e)
+            return jsonify({"resultado": "Error"})
+    else:
+        # imagen en formato base64
+        if "user_image" in request.form:
+            # 9j/4AAQSkZJRgABAQAAAQABAAD/2wBDABALDA4MChAODQ4SERATGCgaGBYWGDEjJR0oOjM9PDkz
+            tamanoarchivo = sys.getsizeof(request.form["user_image"])
+            if tamanoarchivo > app.config["MAX_CONTENT_LENGTH"] or tamanoarchivo <= 0:
+                return jsonify({"resultado": "Error"})
+            file_base64 = request.form["user_image"]
+            api_response = api_instance.image_ocr_post(file_base64)
+            
+            if (api_response.text_result != ""):
+                return {"resultado": api_response.text_result}
+            else:
+                return {"resultado": "Sin Texto"}
 
+            
+        else:
+            return jsonify({"resultado": "Error"})
+        
     return redirect(url_for("home"))
 
 
